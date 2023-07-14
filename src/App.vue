7625734132d0c4a41bc89126dcd13d9943980a2b
adamanr <adamanq@yandex.ru>
2023-07-14 18:28:45 +0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Header from './components/Header.vue';
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <Header/>        
+
+        <!-- <RouterLink to="/">Home</RouterLink> -->
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
